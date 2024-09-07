fe38281f187ea04d67974432f5ccd249db43c219
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2024-09-07 13:50:17 +0200

    watch and watchEffect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref, computed, watch, watchEffect } from "vue";
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  setup() {
+    const search = ref('')
+    const names = ref([
+      "mario",
+      "yoshi",
+      "luigi",
+      "toad",
+      "bowser",
+      "koopa",
+      "peach",
+    ]);
+
+    const stopWatch = watch(search, () => {
+      console.log("watch function ran");
+    })
+
+    const stopEffect = watchEffect(() => {
+      console.log("watchEffect function ran", search.value);
+    })
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value)
+      )
+    })
+
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
+    return { names, search, matchingNames, handleClick };
+  },
+};
 </script>
