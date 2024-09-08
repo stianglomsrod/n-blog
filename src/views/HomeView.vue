a45e2a573a96f6f52bdf945a1f18060b23bf26c8
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2024-09-08 07:30:06 +0200

    Lifecycle hooks with composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,27 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-    <button @click="handleClick">stop watching</button>
+    <PostList :posts="posts" v-if="showPosts"/>
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
 <script>
-import { ref, computed, watch, watchEffect } from "vue";
+import { ref } from "vue";
+import PostList from "../components/PostList.vue"
 export default {
   name: "HomeView",
+  components: { PostList },
   setup() {
-    const search = ref('')
-    const names = ref([
-      "mario",
-      "yoshi",
-      "luigi",
-      "toad",
-      "bowser",
-      "koopa",
-      "peach",
-    ]);
+    const posts = ref([
+      { title: "welcome to the blog", body: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore accusantium magni ex, adipisci voluptas quisquam consectetur cupiditate quos temporibus aperiam doloremque non perspiciatis eveniet dignissimos sint et alias reprehenderit earum libero asperiores voluptatem doloribus sapiente voluptatum! Dignissimos amet, assumenda nam voluptatem aliquam beatae qui nemo harum, necessitatibus ut in nulla quisquam quia dolor distinctio aspernatur eveniet maiores natus inventore illo debitis eos atque tenetur. Suscipit quas tempora officiis nesciunt doloremque deserunt vero dolor aspernatur itaque commodi alias, provident, necessitatibus, consectetur reprehenderit rerum. Voluptatum provident quod, odit deserunt similique aperiam autem laboriosam quas incidunt nesciunt exercitationem consequatur. Ea distinctio eum laborum possimus eius aspernatur a, eos deleniti necessitatibus dignissimos, tempore labore omnis voluptatum eligendi sint, temporibus consequatur amet consectetur perferendis facere fugiat molestias? Dolores animi enim cumque illo accusantium, sed architecto aperiam corrupti commodi modi, voluptatibus sint temporibus repellat, odio distinctio libero perferendis quis? Incidunt vero reprehenderit, eligendi amet inventore corporis adipisci pariatur doloremque temporibus enim quia fugit eum rem obcaecati laboriosam error consequuntur veritatis neque asperiores nostrum molestias? Aut rem, quia nobis ipsam ullam temporibus? Reiciendis consectetur aliquam quasi aut aspernatur necessitatibus doloribus harum. Corrupti blanditiis quo ut excepturi. Adipisci voluptates porro quibusdam eos nemo corporis natus nobis earum repellendus?", id: 1 },
+      { title: "top 5 CSS tips", body: "lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore accusantium magni ex, adipisci voluptas quisquam consectetur cupiditate quos temporibus aperiam doloremque non perspiciatis eveniet dignissimos sint et alias reprehenderit earum libero asperiores voluptatem doloribus sapiente voluptatum! Dignissimos amet, assumenda nam voluptatem aliquam beatae qui nemo harum, necessitatibus ut in nulla quisquam quia dolor distinctio aspernatur eveniet maiores natus inventore illo debitis eos atque tenetur. Suscipit quas tempora officiis nesciunt doloremque deserunt vero dolor aspernatur itaque commodi alias, provident, necessitatibus, consectetur reprehenderit rerum. Voluptatum provident quod, odit deserunt similique aperiam autem laboriosam quas incidunt nesciunt exercitationem consequatur. Ea distinctio eum laborum possimus eius aspernatur a, eos deleniti necessitatibus dignissimos, tempore labore omnis voluptatum eligendi sint, temporibus consequatur amet consectetur perferendis facere fugiat molestias? Dolores animi enim cumque illo accusantium, sed architecto aperiam corrupti commodi modi, voluptatibus sint temporibus repellat, odio distinctio libero perferendis quis? Incidunt vero reprehenderit, eligendi amet inventore corporis adipisci pariatur doloremque temporibus enim quia fugit eum rem obcaecati laboriosam error consequuntur veritatis neque asperiores nostrum molestias? Aut rem, quia nobis ipsam ullam temporibus? Reiciendis consectetur aliquam quasi aut aspernatur necessitatibus doloribus harum. Corrupti blanditiis quo ut excepturi. Adipisci voluptates porro quibusdam eos nemo corporis natus nobis earum repellendus?", id: 2 }
+    ])
 
-    const stopWatch = watch(search, () => {
-      console.log("watch function ran");
-    })
+    const showPosts = ref(true)
 
-    const stopEffect = watchEffect(() => {
-      console.log("watchEffect function ran", search.value);
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value)
-      )
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-
-    return { names, search, matchingNames, handleClick };
+    return { posts, showPosts }
   },
 };
 </script>
